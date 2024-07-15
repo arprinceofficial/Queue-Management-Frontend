9375cc12181313c08c8a95639f8dafa22c304730
arprinceofficial <ashiqurr04@gmail.com>
2024-07-15 11:20:18 +0600

    update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,43 +1,43 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
- runtimeConfig: {
-					public: {
-									apiURL: process.env.NUXT_PUBLIC_API_URL,
-					}
+	runtimeConfig: {
+		public: {
+			apiURL: process.env.NUXT_PUBLIC_API_URL,
+		}
 	},
 
- // css: ['@/assets/tailwindcss.css'],
+	// css: ['@/assets/tailwindcss.css'],
 	css: [
-					'~/assets/css/main.css',
-					'~/assets/fa-6.4.0/css/all.css'
+		'~/assets/css/main.css',
+		'~/assets/fa-6.4.0/css/all.css'
 	],
 
- modules: [
-					'@nuxtjs/tailwindcss',
-					'nuxt-icon',
-					'@nuxtjs/color-mode',
+	modules: [
+		'@nuxtjs/tailwindcss',
+		'nuxt-icon',
+		'@nuxtjs/color-mode',
 	],
 
- colorMode: {
-					preference: 'system', // default value of $colorMode.preference
-					fallback: 'light', // fallback value if not system preference found
-					hid: 'nuxt-color-mode-script',
-					globalName: '__NUXT_COLOR_MODE__',
-					componentName: 'ColorScheme',
-					classPrefix: '',
-					classSuffix: '',
-					storageKey: 'nuxt-color-mode'
+	colorMode: {
+		preference: 'system', // default value of $colorMode.preference
+		fallback: 'light', // fallback value if not system preference found
+		hid: 'nuxt-color-mode-script',
+		globalName: '__NUXT_COLOR_MODE__',
+		componentName: 'ColorScheme',
+		classPrefix: '',
+		classSuffix: '',
+		storageKey: 'nuxt-color-mode'
 	},
 
- vite: {
-					css: {
-							preprocessorOptions: {
-									scss: {
-											additionalData: '@use "@/assets/scss/themes-basic-v1/_style.scss" as *;'
-									}
-							}
-					}
-	  },
+	vite: {
+		css: {
+			preprocessorOptions: {
+				scss: {
+					additionalData: '@use "@/assets/scss/themes-basic-v1/_style.scss" as *;'
+				}
+			}
+		}
+	},
 
- compatibilityDate: '2024-07-15',
+	compatibilityDate: '2024-07-15',
 });
