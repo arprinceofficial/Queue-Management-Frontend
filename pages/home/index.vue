497ae1485f9519984313d7fad5017980b48543fa
arprinceofficial <ashiqurr04@gmail.com>
2024-07-25 15:26:41 +0600

    fix button pages\home\index.vue

--- pages/home/index.vue
+++ pages/home/index.vue
@@ -17,7 +17,7 @@
                     <div class="flex items-center">
                         <span class="text-[#637381] text-[48px]">to&nbsp;</span> <span class="text-[#0070C0] text-[54px] font-bold">Digital</span> <span class="text-[#00B076] text-[54px] font-semibold">Bangsamoro</span>
                     </div>
-                    <ButtonPrimary class="mt-[200px] w-[420px]">
+                    <ButtonPrimary class="mt-[200px] w-[420px] h-[85px] text-[32px]">
 						Get Started
 					</ButtonPrimary>
 				</div>
