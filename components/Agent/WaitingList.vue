63ff6600c026436044d79840deb88fbcf8fddf1b
arprinceofficial <ashiqurr04@gmail.com>
2024-07-28 16:24:25 +0600

    2xl: for agent-panel

--- components/Agent/WaitingList.vue
+++ components/Agent/WaitingList.vue
@@ -10,11 +10,11 @@
     <!-- <pre>{{ data }}</pre> -->
     <div v-for="(item, index) in data" :key="index" class="p-3 rounded-[3px] bg-[#FFF] border-[5px] border-y-0 border-[solid] border-[#EC9C0E] mb-3 flex items-center justify-between">
         <div class="flex">
-            <p class="pr-5 text-[13px] md:text-[15px] 2xl:text-[20px] font-semibold">Token No : {{ item.token_no }}</p>
-            <div class="flex gap-3 text-[13px] md:text-[15px] 2xl:text-[20px] font-semibold">
+            <p class="pr-5 text-[13px] md:text-[15px] font-semibold">Token No : {{ item.token_no }}</p>
+            <div class="flex gap-3 text-[13px] md:text-[15px] font-semibold">
                 <p>Services : <span class="text-[#EC9C0E]">{{ item.service_name }}</span></p>
             </div>
         </div>
-        <p class="text-[10px] md:text-[13px] 2xl:text-[17px] text-[#535353]">Token Generated on {{ item.token_generated }}</p>
+        <p class="text-[10px] md:text-[13px] text-[#535353]">Token Generated on {{ item.token_generated }}</p>
     </div>
 </template>
--- pages/agent-panel/index.vue
+++ pages/agent-panel/index.vue
@@ -27,20 +27,20 @@
             <div class="md:w-1/2 w-full h-full flex justify-center bg-[#DFEFE8]">
                 <div class="p-2 w-full flex flex-col items-center justify-between">
                     <div class="flex flex-col items-center overflow-auto w-full">
-                        <p class="text-[18px] sm:text-[20px] md:text-[30px] 2xl:text-[40px] text-[#00B076] text-center font-semibold">
+                        <p class="text-[18px] sm:text-[20px] md:text-[30px] text-[#00B076] text-center font-semibold">
                             Currently Serving
                         </p>
-                        <h1 class="text-[28px] sm:text-[35px] md:text-[45px] 2xl:text-[70px] text-black font-semibold">
+                        <h1 class="text-[28px] sm:text-[35px] md:text-[45px] text-black font-semibold">
                             Token Number
                         </h1>
-                        <div class="w-[130px] sm:w-[230px] md:w-[350px] 2xl:w-[460px] h-[70px] sm:h-[100px] md:h-[150px] 2xl:h-[200px] rounded-[20px] border-[2px] md:border-[4px] 2xl:border-[4px] border-[solid] border-[#0083C4] bg-[#FFF] flex items-center justify-center mt-3">
-                            <h1 class="text-[30px] sm:text-[60px] md:text-[80px] 2xl:text-[120px] text-[#0083C4] text-center not-italic font-semibold leading-[normal]">
+                        <div class="w-[130px] sm:w-[230px] md:w-[350px] h-[70px] sm:h-[100px] md:h-[150px] rounded-[20px] border-[2px] md:border-[4px] border-[solid] border-[#0083C4] bg-[#FFF] flex items-center justify-center mt-3">
+                            <h1 class="text-[30px] sm:text-[60px] md:text-[80px] text-[#0083C4] text-center not-italic font-semibold leading-[normal]">
                                 B-012
                             </h1>
                         </div>
                         <div class="flex flex-col items-center mt-3">
-                            <p class="text-[14px] sm:text-[15px] md:text-[20px] 2xl:text-[30px] font-semibold">Serving Time</p>
-                            <p class="text-[25px] sm:text-[30px] md:text-[40px] 2xl:text-[70px] font-semibold">00:45:13</p>
+                            <p class="text-[14px] sm:text-[15px] md:text-[20px] font-semibold">Serving Time</p>
+                            <p class="text-[25px] sm:text-[30px] md:text-[40px] font-semibold">00:45:13</p>
                         </div>
                     </div>
 
@@ -60,11 +60,11 @@
             <div class="md:w-1/2 w-full h-full overflow-auto flex justify-center ">
                 <div class="pt-2 md:p-2 md:py-0 w-full flex flex-col items-center justify-between">
                     <div class="w-full px-5 py-[12px] bg-[#F2E5C5]">
-                        <p class="text-[20px] md:text-[25px] 2xl:text-[30px] font-semibold">
+                        <p class="text-[20px] md:text-[25px] font-semibold">
                             Waiting
                         </p>
                     </div>
-                    <div class="h-full w-full p-3 overflow-auto bg-[#FBF7F1] sm:max-h-[calc(100vh-95px)] md:max-h-[calc(100vh-95px)] 2xl:max-h-[calc(100vh-95px)]">
+                    <div class="h-full w-full p-3 overflow-auto bg-[#FBF7F1] sm:max-h-[calc(100vh-95px)] md:max-h-[calc(100vh-95px)]">
                         <AgentWaitingList :data="waiting_list" />
                     </div>
 
