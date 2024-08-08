5e5c0491c3c1401b5da136384fc02852a378f17f
arprinceofficial <ashiqurr04@gmail.com>
2024-08-09 04:36:43 +0600

    add 5 more api for agent
    1. getWaitingList
    2. reserveQueue
    3. getReserveQueue
    4 .completeQueue
    5. cancelQueue

--- plugins/api-endpoints.ts
+++ plugins/api-endpoints.ts
@@ -2,19 +2,19 @@ export default defineNuxtPlugin(() => {
     return {
         provide: {
             // Office API Endpoints
-            api_office_queue_services: 'queue-services',
-            api_office_queue_vouchers_store :'queue-services-create',
-            api_office_queue_vouchers_waiting_screen: 'queue-vouchers/waiting-screen',
+            api_office_queue_services: 'queue-services', // Done
+            api_office_queue_vouchers_store :'queue-services-create', // Done
+            api_office_queue_vouchers_waiting_screen: 'queue-vouchers/waiting-screen', // Not Done
 
             // Agent API Endpoints
-            api_agent_info: 'info',
-            api_agent_queue_counters_get_counter: 'get-counter',
-            api_agent_queue_counters_set_counter: 'set-counter',
-            api_agent_queue_counters_revoke_counter: 'revoke-counter',
-            api_agent_queue_vouchers_get_waitinglist: 'queue-vouchers/get-waitinglist',
-            api_agent_queue_vouchers_reserve_queue: 'queue-vouchers/reserve-queue',
-            api_agent_queue_vouchers_get_reserve_queue: 'queue-vouchers/get-reserve-queue',
-            api_agent_queue_vouchers_complete_queue: 'queue-vouchers/complete-queue',
+            api_agent_info: 'current-user', // Done
+            api_agent_queue_counters_get_counter: 'get-counter', // Done
+            api_agent_queue_counters_set_counter: 'set-counter', // Done
+            api_agent_queue_counters_revoke_counter: 'revoke-counter', // Done
+            api_agent_queue_vouchers_get_waitinglist: 'get-waiting-list', // Done
+            api_agent_queue_vouchers_reserve_queue: 'reserve-queue', // Done
+            api_agent_queue_vouchers_get_reserve_queue: 'get-reserve-queue', // Done
+            api_agent_queue_vouchers_complete_queue: 'complete-queue', // Done
             api_agent_queue_counters_get_online_counter: 'queue-counters/get-online-counter',
             api_agent_queue_vouchers_tranfer_queue: 'queue-vouchers/tranfer-queue',
         }
