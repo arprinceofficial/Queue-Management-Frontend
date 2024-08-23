ef42a0b078f2eb43fc65611a26ab46f0c977e3c4
arprinceofficial <ashiqurr04@gmail.com>
2024-08-24 00:52:25 +0600

    handle sidebar hamburger for admin user

--- components/AppHeader/Admin.vue
+++ components/AppHeader/Admin.vue
@@ -21,6 +21,8 @@
     onMounted(() => {
         getProfileImage();
     });
+
+    const hamburger_button = useState('hamburger_button');
 </script>
 <template>
     <!-- <pre>{{ admin_user }}</pre> -->
@@ -29,6 +31,21 @@
         <div class="mx-auto px-4 sm:px-6 lg:px-8 h-full">
             <div class="flex justify-between h-full">
                 <div class="flex">
+                    <button @click="hamburger_button = !hamburger_button" v-if="!hamburger_button"
+                        class="inline-flex  items-center justify-center p-2 rounded-md text-gray-400 dark:text-gray-500 hover:text-gray-500 dark:hover:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-900 focus:outline-none focus:bg-gray-100 dark:focus:bg-gray-900 focus:text-gray-500 dark:focus:text-gray-400 transition duration-150 ease-in-out">
+                        <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                            <path :class="{
+                                hidden: hamburger_button,
+                                'inline-flex': !hamburger_button
+                            }" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M4 6h16M4 12h16M4 18h16" />
+                            <path :class="{
+                                hidden: !hamburger_button,
+                                'inline-flex': hamburger_button
+                            }" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M6 18L18 6M6 6l12 12" />
+                        </svg>
+                    </button>
                     <!-- Logo -->
                     <div class="shrink-0 flex items-center">
                         <NuxtLink href="/">
