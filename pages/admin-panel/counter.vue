c107aade2c375d63810781c1634a0c5bfcf955bd
arprinceofficial <ashiqurr04@gmail.com>
2024-08-26 17:57:39 +0600

    make pagination for counter.vue

--- pages/admin-panel/counter.vue
+++ pages/admin-panel/counter.vue
@@ -4,27 +4,64 @@
     const { 
         $api_admin_counter_list,
         $api_admin_counter_delete,
+        $api_admin_office_list,
     } = useNuxtApp();
 
+    const admin_office_list = useState('admin_office_list', () => []);
+    const loadOfficeList = async () => {
+        // if(admin_office_list.value.length > 0) return;
+        try{
+            const getData = await $fetchAdmin($api_admin_office_list, {
+                method: 'GET',
+            });
+            admin_office_list.value = getData.data;
+        } catch(e){
+            console.log('Get Message',e.message);
+        }
+    }
+    onMounted(() => {
+        loadOfficeList();
+    });
+
+    const route = useRoute();
+    const status = ref('');
+    const search = ref('');
+    const office_id = ref('');
+    const pagination_config = ref({
+        data: [],
+        lang: 'en',
+        align: 'center',
+        action: ''
+    });
     const loader = ref(false);
     const admin_counter_list = useState('admin_counter_list', () => []);
     const loadCounterList = async () => {
         loader.value = true;
         try{
             const getData = await $fetchAdmin($api_admin_counter_list, {
-                method: 'GET',
+                method: 'POST',
+                body: {
+                    page: route.query.page ? route.query.page : 1,
+                    limit: 10,
+                    search: search.value,
+                    status: status.value,
+                    office_id: office_id.value,
+                },
             });
             admin_counter_list.value = getData.data;
+            pagination_config.value.data = getData.pagination;
         } catch(e){
             console.log('Get Message',e.message);
         } finally {
             loader.value = false;
         }
     }
-
     onMounted(() => {
         loadCounterList();
     });
+    watch(() => route.query, (to) => {
+        loadCounterList();
+    })
 
     // Counter Add Edit Modal Handler
     const data = ref({});
@@ -36,7 +73,8 @@
 
         modal_title.value == 'Add' ? 
         admin_counter_list.value.push(data) :
-        admin_counter_list.value = admin_counter_list.value.map(item => item.id == data.id ? data : item); 
+        loadCounterList();
+        // admin_counter_list.value = admin_counter_list.value.map(item => item.id == data.id ? data : item); 
     }
     const addCounter = () => {
         data.value = {};
@@ -81,15 +119,38 @@
             <div class="flex h-full">
                 <AdminLeftSide />
                 <!-- Right Side -->
-                <LoaderSpinkitBounceLoader v-if="loader" class="w-full"/>
-                <div v-else class="h-full w-full overflow-auto">
-                    <div class="w-full flex justify-end pt-5 px-8">
+                <div class="h-full w-full overflow-auto">
+                    <div class="w-full flex justify-between items-center pt-5 px-8 mb-4">
+                        <div class="flex justify-between items-center">
+                            <div class="flex items-center gap-4">
+                                <div class="flex items-center gap-2">
+                                    <label for="search" class="text-[#4D5155] dark:text-gray-200">Search</label>
+                                    <input type="text" id="search" class="border border-gray-200 rounded-lg px-3 py-2" v-model="search" @input="loadCounterList">
+                                </div>
+                                <div class="flex items-center gap-3">
+                                    <label for="status" class="text-[#4D5155] dark:text-gray-200">Status</label>
+                                    <select v-model="status" @change="loadCounterList" id="status" class="border border-gray-200 rounded-lg px-3 py-2 pr-8">
+                                        <option value="">All</option>
+                                        <option value="1">Active</option>
+                                        <option value="0">Inactive</option>
+                                    </select>
+                                </div>
+                                <div class="flex items-center gap-3">
+                                    <label for="office_id" class="text-[#4D5155] dark:text-gray-200">Office</label>
+                                    <select v-model="office_id" @change="loadCounterList" id="office_id" class="border border-gray-200 rounded-lg px-3 py-2 pr-8">
+                                        <option value="">All</option>
+                                        <option v-for="(item, index) in admin_office_list" :key="index" :value="item.id">{{ item.office_name }}</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
                         <button @click="addCounter" class="bg-[#0083C4] text-white py-2 px-4 rounded-lg ml-4 mb-4">
                             <i class="fas fa-plus pr-1"></i>
                             Add Counter
                         </button>
                     </div>
-                    <div class="px-6 pb-2 flex flex-col justify-between w-full">
+                    <LoaderSpinkitBounceLoader v-if="loader" class="w-full"/>
+                    <div v-else class="px-6 pb-2 flex flex-col justify-between w-full">
                         <div class="mt-4 border border-gray-200 rounded-lg">
                             <div class="border-b border-gray-200">
                                 <h4 class="text-[18px] font-semibold dark:text-gray-200 py-2 px-4">Counter List</h4>
@@ -170,6 +231,7 @@
                                     </table>
                                 </div>
                             </div>
+                            <Pagination class="px-4" :config="pagination_config"/>
                         </div>
                     </div>
                 </div>
