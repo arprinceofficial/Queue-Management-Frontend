a0171e02cac4ecda4e670ace4a1b1c89007ac5b8
arprinceofficial <ashiqurr04@gmail.com>
2024-08-26 16:31:17 +0600

    make pagination for wt-news.vue

--- pages/admin-panel/wt-news.vue
+++ pages/admin-panel/wt-news.vue
@@ -6,25 +6,43 @@
         $api_admin_wt_news_delete,
     } = useNuxtApp();
 
+    const route = useRoute();
+    const status = ref('');
+    const search = ref('');
+    const pagination_config = ref({
+        data: [],
+        lang: 'en',
+        align: 'center',
+        action: ''
+    });
     const loader = ref(false);
     const admin_news_list = useState('admin_news_list', () => []);
     const loadNewsList = async () => {
         loader.value = true;
         try{
             const getData = await $fetchAdmin($api_admin_wt_news_list, {
-                method: 'GET',
+                method: 'POST',
+                body: {
+                    page: route.query.page ? route.query.page : 1,
+                    limit: 10,
+                    search: search.value,
+                    status: status.value,
+                },
             });
             admin_news_list.value = getData.data;
+            pagination_config.value.data = getData.pagination;
         } catch(e){
             console.log('Get Message',e.message);
         } finally {
             loader.value = false;
         }
     }
-
     onMounted(() => {
         loadNewsList();
     });
+    watch(() => route.query, (to) => {
+        loadNewsList();
+    })
 
     // News Add Edit Modal Handler
     const data = ref({});
@@ -36,7 +54,8 @@
 
         modal_title.value == 'Add' ? 
         admin_news_list.value.push(data) :
-        admin_news_list.value = admin_news_list.value.map(item => item.id == data.id ? data : item); 
+        loadNewsList();
+        // admin_news_list.value = admin_news_list.value.map(item => item.id == data.id ? data : item); 
     }
     const addNews = () => {
         data.value = {};
@@ -81,15 +100,31 @@
             <div class="flex h-full">
                 <AdminLeftSide />
                 <!-- Right Side -->
-                <LoaderSpinkitBounceLoader v-if="loader" class="w-full"/>
-                <div v-else class="h-full w-full overflow-auto">
-                    <div class="w-full flex justify-end pt-5 px-8">
+                <div class="h-full w-full overflow-auto">
+                    <div class="w-full flex justify-between items-center pt-5 px-8 mb-4">
+                        <div class="flex justify-between items-center">
+                            <div class="flex items-center gap-4">
+                                <div class="flex items-center gap-2">
+                                    <label for="search" class="text-[#4D5155] dark:text-gray-200">Search</label>
+                                    <input type="text" id="search" class="border border-gray-200 rounded-lg px-3 py-2" v-model="search" @input="loadNewsList">
+                                </div>
+                                <div class="flex items-center gap-3">
+                                    <label for="status" class="text-[#4D5155] dark:text-gray-200">Status</label>
+                                    <select v-model="status" @change="loadNewsList" id="status" class="border border-gray-200 rounded-lg px-3 py-2 pr-8">
+                                        <option value="">All</option>
+                                        <option value="1">Active</option>
+                                        <option value="0">Inactive</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
                         <button @click="addNews" class="bg-[#0083C4] text-white py-2 px-4 rounded-lg ml-4 mb-4">
                             <i class="fas fa-plus pr-1"></i>
                             Add News
                         </button>
                     </div>
-                    <div class="px-6 pb-2 flex flex-col justify-between w-full">
+                    <LoaderSpinkitBounceLoader v-if="loader" class="w-full"/>
+                    <div v-else  class="px-6 pb-2 flex flex-col justify-between w-full">
                         <div class="mt-4 border border-gray-200 rounded-lg">
                             <div class="border-b border-gray-200">
                                 <h4 class="text-[18px] font-semibold dark:text-gray-200 py-2 px-4">News List</h4>
@@ -130,7 +165,7 @@
                                             <tr v-for="(item, index) in admin_news_list" :key="index">
                                                 <td class="sticky left-0">
                                                     <div class="flex justify-center items-center">
-                                                        {{ index + 1 }}
+                                                        {{ (pagination_config.data.current_page - 1) * pagination_config.data.per_page + index + 1 }}
                                                     </div>
                                                 </td>
                                                 <td>{{ item.title }}</td>
@@ -158,6 +193,7 @@
                                     </table>
                                 </div>
                             </div>
+                            <Pagination class="px-4" :config="pagination_config"/>
                         </div>
                     </div>
                 </div>
