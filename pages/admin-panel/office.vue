e34a2736eac5d5efab637a103e6e559c23513176
arprinceofficial <ashiqurr04@gmail.com>
2024-08-30 04:06:31 +0600

    add validation for office

--- pages/admin-panel/office.vue
+++ pages/admin-panel/office.vue
@@ -69,6 +69,7 @@
     }
 
     // Counter Delete Handler
+    const response_modal = ref({});
     const is_loading = ref(false);
     const deleteOffice = async (id) => {
         const confirm = window.confirm('Are you sure you want to delete this item?');
@@ -79,11 +80,24 @@
                 method: 'POST',
                 body: {id: id},
             });
+            response_modal.value = getData;
             if (getData.status == true) {
                 admin_office_list.value = admin_office_list.value.filter(item => item.id != id);
             }
         } catch(e){
             console.log('Get Message',e.message);
+            if (!e.response?.status){
+                response_modal.value = {
+                    status: false,
+                    message: 'Something went wrong. Please try again later.',
+                }
+            } else {
+                response_modal.value = {
+                    status: e.response._data.status,
+                    message: e.response._data.message,
+                }
+                
+            }
         } finally {
             is_loading.value = false;
         }
@@ -201,5 +215,6 @@
             @cancel="cancelModal"
         />
         <LoaderModalSpin :isOpen="is_loading" />
+        <ResponseModal :response_modal="response_modal" />
     </div>
 </template>
