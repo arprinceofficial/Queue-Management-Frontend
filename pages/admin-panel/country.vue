fbe4397ca1b8b16fc4aefd1004a9c2e3f81adf93
arprinceofficial <ashiqurr04@gmail.com>
2024-08-30 17:44:31 +0600

    add validations for Country

--- pages/admin-panel/country.vue
+++ pages/admin-panel/country.vue
@@ -85,6 +85,7 @@
     }
 
     // Counter Delete Handler
+    const response_modal = ref({});
     const is_loading = ref(false);
     const deleteCountry = async (id) => {
         const confirm = window.confirm('Are you sure you want to delete this item?');
@@ -95,11 +96,23 @@
                 method: 'POST',
                 body: {id: id},
             });
+            response_modal.value = getData;
             if (getData.status == true) {
                 admin_country_list.value = admin_country_list.value.filter(item => item.id != id);
             }
         } catch(e){
             console.log('Get Message',e.message);
+            if (!e.response?.status){
+                response_modal.value = {
+                    status: false,
+                    message: 'Something went wrong. Please try again later.',
+                }
+            } else {
+                response_modal.value = {
+                    status: e.response._data.status,
+                    message: e.response._data.message,
+                }
+            }
         } finally {
             is_loading.value = false;
         }
@@ -238,5 +251,6 @@
             @cancel="cancelModal"
         />
         <LoaderModalSpin :isOpen="is_loading" />
+        <ResponseModal :response_modal="response_modal" />
     </div>
 </template>
