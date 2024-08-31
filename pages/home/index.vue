bc3ee23b3d1db67c92c5687c6929dc0641720827
arprinceofficial <ashiqurr04@gmail.com>
2024-08-31 22:05:57 +0600

    update pages\home\index.vue

--- pages/home/index.vue
+++ pages/home/index.vue
@@ -15,7 +15,7 @@
                         Welcome
                     </h2>
                     <div class="flex items-center">
-                        <span class="text-[#637381] text-[48px]">to&nbsp;</span> <span class="text-[#0070C0] text-[54px] font-bold">Digital</span> <span class="text-[#00B076] text-[54px] font-semibold">Bangsamoro</span>
+                        <span class="text-[#637381] text-[48px]">to&nbsp;</span> <span class="text-[#0070C0] text-[54px] font-bold">Digital</span> <span class="text-[#00B076] text-[54px] font-semibold">Queue</span>
                     </div>
                     <NuxtLink to="/service" class="mt-[200px] ">
 						<ButtonPrimary class="w-[420px] h-[85px] text-[32px]">
